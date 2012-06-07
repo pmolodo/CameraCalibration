b6c4ec065a24ed50fa3885433e47786bafa19099
Paul Molodowitch <elrond79@gmail.com>
2012-06-07 15:22:11 -0700

    convert default.xml to default.yaml; make yaml the default configuration file

--- calibrate.cpp
+++ calibrate.cpp
@@ -235,7 +235,7 @@ int main(int argc, char* argv[])
 {
     help();
     Settings s;
-    const string inputSettingsFile = argc > 1 ? argv[1] : "default.xml";
+    const string inputSettingsFile = argc > 1 ? argv[1] : "default.yaml";
     FileStorage fs(inputSettingsFile, FileStorage::READ); // Read the settings
     if (!fs.isOpened())
     {
